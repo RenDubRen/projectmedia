e9b6db0503177555ec9a2d52af937d69053b0125
unknown <romka_grand@mail.ru>
2019-08-09 20:09:05 +1000

    Добавлена отправка и проверка формы через AJAX

---
+++ src/js/scripts.js
@@ -0,0 +1,15 @@
+// Отправка и обработка формы через AJAX
+$('#offer-form').on('submit', function(event) {
+    event.preventDefault();
+    $.ajax({
+        url: 'mail.php',
+        type: 'POST',
+        data: $(this).serialize(),
+        success: function(data) {
+            $('.success').html(data + ', Ваша форма отправлена!')
+        },
+        error: function(jqXHR, textStatus) {
+            console.log(jqXHR + ': ' + textStatus);
+        }
+    });
+});
