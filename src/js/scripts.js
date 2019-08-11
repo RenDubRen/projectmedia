5c1458f86b1fb3ddc4b682ba3281079d22c95e32
unknown <romka_grand@mail.ru>
2019-08-11 20:29:31 +1000

    Сверстал секцию Portfolio, добавил слайдер slick и адаптировал под маленькие размеры экрана

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,31 @@
+// Portfolio -- slick script
+$(document).ready(function() {
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+});
+
+
+
 // Отправка и обработка формы через AJAX
 $('#offer-form').on('submit', function(event) {
     event.preventDefault();
