70d987549f040f6e36e7ea5547056b10759fdd5a
unknown <romka_grand@mail.ru>
2019-08-19 23:00:21 +1000

    Сверстал и адаптировал секцию brif, добавил валидацию полей на сайте

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,62 @@
 // Portfolio -- slick script
 $(document).ready(function() {
+    // Валидация формы offer
+    $('#offer-form').validate({
+        rules: {
+            username: {
+                required: true,
+                minlength: 2,
+                maxlength: 15
+            },
+            phone: {
+                required: true
+            }
+        },
+        messages: {
+            username: {
+                required: 'Укажите имя',
+                minlength: jQuery.validator.format('Осталось символов: {0}'),
+                maxlength: jQuery.validator.format('Имя слишком длинное')
+            },
+            phone: {
+                required: 'Укажите свой телефон'
+            }
+        }
+    });
+    // Валидация формы brif
+    $('#brif-form').validate({
+        rules: {
+            username: {
+                required: true,
+                minlength: 2,
+                maxlength: 15
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            phone: {
+                required: true
+            }
+        },
+        messages: {
+            username: {
+                required: 'Укажите имя',
+                minlength: jQuery.validator.format('Осталось символов: {0}'),
+                maxlength: jQuery.validator.format('Имя слишком длинное')
+            },
+            email: {
+                required: 'Введите Ваш email',
+                email: 'Введите корректный email'
+            },
+            phone: {
+                required: 'Укажите свой телефон'
+            }
+        }
+    });
+    // Маска телефона
+    $('.phone').mask('+7 (999) 999-99-99');
+    // Slider
     $('.slider').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
